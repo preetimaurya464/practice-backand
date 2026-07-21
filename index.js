ba79938f1e4802a7dc5ddee432c0790f20a4ea7e
preetimaurya464 <preetimaurya227799@gmail.com>
2026-07-21 13:07:10 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require('express');
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send("Hello send");
+});
+app.get('/home', (req,res)=>
+{
+    res.send('my name');
+});
+app.get('/about',(req,res)=>
+{
+    res.send('welcome my project');
+});
+app.get('/login',(req,res)=>
+{
+    res.send('login');
+});
+app.get('/chai',(req,res)=>
+{
+    res.send('chai');
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`);
+});
